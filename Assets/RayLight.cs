05b7247c4857e168d6b30800399b5226b7d41638
Letosim <95633405+Letosim@users.noreply.github.com>
2024-12-08 18:29:44 +0100

    Update RayLight.cs

--- Assets/RayLight.cs
+++ Assets/RayLight.cs
@@ -10,8 +10,6 @@ public class RayLight : MonoBehaviour
 
    public void Draw(Vector3 positionA, Vector3 positionB , int depth, int maxDepth)
    {   
-      
-   
        r = distance + distance;
        g = r / distance;
        b = r + g;
@@ -21,27 +19,22 @@ public class RayLight : MonoBehaviour
      r = r / magnitude;
      g = g / magnitude;
      b = b / magnitude;
-     a = f1-((float) depth / (float) maxDepth);
+
+     a = (float) depth / (float) maxDepth;
+
      Debug.DrawLine(positionA, positionB, new Color(r,g,b,a);
    }
    
 
-public void Draw(Vector3 positionA, Vector3 positionB, Vector3 color)
+public void Draw(Vector3 positionA, Vector3 positionB)
 {
- Debug.DrawLine(positionA, positionB, color *   .1f);
+   Debug.DrawLine(positionA, positionB, color);
 }
 
 
 public void Draw(Vector3 positionA, Vector3 positionB, Vector3 color)
-{
+   {
  Debug.DrawLine(positionA, positionB, color * (f1-((float) depth / (float) maxDepth))  );
 
-}
-
-
-
-//light
-
-
-
+   }
 }
