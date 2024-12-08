94b0aa14ebbd78b9b0ec5ddf702abfc62d03727d
Letosim <95633405+Letosim@users.noreply.github.com>
2024-12-08 07:22:30 +0100

    Update RayLight.cs

--- Assets/RayLight.cs
+++ Assets/RayLight.cs
@@ -6,4 +6,8 @@ public class RayLight : MonoBehaviour
 {
     public float distance;
     public Vector3 color;
+
+    color = distance + distance
+    color = distance - distance
+    color = color * color
 }
